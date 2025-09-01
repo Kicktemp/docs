736a221dbcd6dc74215b32afed986fa739c19972
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2025-09-01 13:20:48 +0200

    add plg_task_kickerechttexte

--- .vitepress/sidebar.js
+++ .vitepress/sidebar.js
@@ -1,5 +1,7 @@
 import KickYooAddonsSidebar from '../src/kicktemp-addons-for-yootheme-pro/sidebar.json';
+import PluginsSidebar from '../src/plugins/sidebar.json';
 
 export default {
     '/kicktemp-addons-for-yootheme-pro/': KickYooAddonsSidebar,
+    '/plugins/': PluginsSidebar,
 };
