bd10d0fe5ec74e6c1d96892d903df67d582b3087
Niels Nübel <niels@kicktemp.com>
2026-02-12 23:35:47 +0100

    bannerstats

--- .vitepress/sidebar.js
+++ .vitepress/sidebar.js
@@ -1,7 +1,9 @@
 import KickYooAddonsSidebar from '../src/kicktemp-addons-for-yootheme-pro/sidebar.json';
 import PluginsSidebar from '../src/plugins/sidebar.json';
+import KickBannerStatsSidebar from '../src/components/com_kickbannerstats/sidebar.json';
 
 export default {
     '/kicktemp-addons-for-yootheme-pro/': KickYooAddonsSidebar,
     '/plugins/': PluginsSidebar,
+    '/components/com_kickbannerstats/': KickBannerStatsSidebar,
 };
